26201f217f1e738b586a3261fca44014509816ab
Whynchu <samuelboxold@gmail.com>
2026-08-09 17:57:42 -0400

    Avoid animation reset parameter shadowing

--- tiny-demons/scripts/player_animation_component.gd
+++ tiny-demons/scripts/player_animation_component.gd
@@ -8,11 +8,11 @@ var frame := 0
 var timer := 0.0
 
 
-func play(new_name: StringName, reset := true) -> void:
+func play(new_name: StringName, restart := true) -> void:
 	if animation_name != new_name:
 		animation_name = new_name
 		animation_changed.emit(animation_name)
-	if reset:
+	if restart:
 		frame = 0
 		timer = 0.0
 
